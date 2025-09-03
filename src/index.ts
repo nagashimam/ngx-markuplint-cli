06cc110b7d6e5886cf426f005ecb0cfbd8942ac4
nagashimam <masato.ngsm@gmail.com>
2025-09-03 19:53:37 +0900

    feat: Read config from .vscode/settings.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { runMarkuplintAgainstTemplateFile } from "ngx-html-bridge-markuplint";
+import { readFileSync } from "node:fs";
 
 const files: string[] = process.argv.slice(2);
 
@@ -10,8 +11,15 @@ if (!files || files.length < 1) {
 }
 
 const run = async () => {
+	const settingsJson = readFileSync("./.vscode/settings.json", "utf8");
+	const settings = JSON.parse(settingsJson);
+	const includedAttributes = settings?.ngxMarkuplint
+		? settings.ngxMarkuplint.includedAttributes
+		: settings["ngxMarkuplint.includedAttributes"];
 	const promises = files.map(async (file: string) => {
-		const results = await runMarkuplintAgainstTemplateFile(file);
+		const results = await runMarkuplintAgainstTemplateFile(file, {
+			includedAttributes,
+		});
 		return results.map((result) => {
 			const violations = result.violations.map(
 				(violation) => (violation as any).message,
